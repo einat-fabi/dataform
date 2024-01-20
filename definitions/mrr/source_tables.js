f94711c098c15c3c0acdd079144eddb2c5e1ee47
Einat  Lazarovich <einatl@fire-arc.com>
2024-01-20 17:00:21 +0000

    updates

---
+++ definitions/mrr/source_tables.js
@@ -0,0 +1,30 @@
+declare({
+    database: "data-processing-272107",
+    schema: "stg_dev",
+    name: "fact_all_searches_user_id"
+});
+
+declare({
+    database: "data-processing-272107",
+    schema: "mrr_dev",
+    name: "partners_data"
+});
+
+declare({
+    database: "data-processing-272107",
+    schema: "mrr",
+    name: "app_installmac"
+});
+
+// declare({
+//     database: "pc-windows-168009",
+//     schema: "report",
+//     name: "search_log"
+// });
+
+// config {
+//     type: "declaration",
+//     database: "data-processing-272107",
+//     schema: "mrr",
+//     name: "app_installmac"
+// }
