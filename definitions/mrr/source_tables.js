c026482f9e9b7cd2b2b15a5aee6660a17bb42a8a
Einat  Lazarovich <einatl@fire-arc.com>
2024-01-21 21:49:11 +0000

    more tables

--- definitions/mrr/source_tables.js
+++ definitions/mrr/source_tables.js
@@ -4,6 +4,32 @@ declare({
     name: "fact_all_searches_user_id"
 });
 
+
+declare({
+    database: "search-somoto-hrd",
+    schema: "all_searches",
+    name: "search_log_201408"
+});
+
+
+declare({
+    database: "pc-windows-168009",
+    schema: "report",
+    name: "search_log"
+});
+
+declare({
+    database: "pc-windows-168009",
+    schema: "report",
+    name: "client_installation_log"
+});
+
+declare({
+    database: "pc-windows-168009",
+    schema: "report",
+    name: "download_activity"
+});
+
 declare({
     database: "data-processing-272107",
     schema: "mrr_dev",
